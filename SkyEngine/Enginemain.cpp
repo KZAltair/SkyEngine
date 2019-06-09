3cfd4e779972dc2fc9259873ba98a40bace8b133
Ivan M <massyutin@gmail.com>
2019-06-09 19:31:47 +0600

    WndProc with Quit msg added

--- SkyEngine/Enginemain.cpp
+++ SkyEngine/Enginemain.cpp
@@ -1,5 +1,17 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) //Returns a 32bit int value
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	}
+	return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+
 //Windows entrypoint
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -9,7 +21,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hbrBackground = nullptr;
@@ -27,8 +39,21 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 		200, 200, 640, 480, nullptr, nullptr, hInstance, nullptr);
 
 	ShowWindow(hwnd, SW_SHOW);
-	//Infine loop to test all stuff with windows
-	//TODO remove or rework this stuff
-	while (true);
-	return 0;
+
+	MSG msg;
+	BOOL gResult;
+
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
