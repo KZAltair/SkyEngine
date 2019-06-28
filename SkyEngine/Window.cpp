820553897ab7fa998b166094326b269f46a04b11
Ivan M <massyutin@gmail.com>
2019-06-28 18:37:31 +0600

    Added error checking handling for D3D and DXGI

--- SkyEngine/Window.cpp
+++ SkyEngine/Window.cpp
@@ -88,7 +88,7 @@ void Window::SetTitle(const std::string & title)
 	}
 }
 
-std::optional<int> Window::ProcessMessages()
+std::optional<int> Window::ProcessMessages() noexcept
 {
 	MSG msg;
 	while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
@@ -108,6 +108,10 @@ std::optional<int> Window::ProcessMessages()
 
 Graphics & Window::Gfx()
 {
+	if (!pGfx)
+	{
+		throw SKYENGINE_NOGFX_EXCEPT();
+	}
 	return *pGfx;
 }
 
@@ -235,52 +239,64 @@ LRESULT Window::HandleMsg(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) noe
 	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
 
-Window::Exception::Exception(int line, const char * file, HRESULT hr) noexcept
-	:
-	ExceptionManager(line, file),
-	hr(hr)
+std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
 {
+	char* pMsgBuf = nullptr;
+	// windows will allocate memory for err string and make our pointer point to it
+	const DWORD nMsgLen = FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		nullptr, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		reinterpret_cast<LPSTR>(&pMsgBuf), 0, nullptr
+	);
+	// 0 string length returned indicates a failure
+	if (nMsgLen == 0)
+	{
+		return "Unidentified error code";
+	}
+	// copy error string from windows-allocated buffer to std::string
+	std::string errorString = pMsgBuf;
+	// free windows buffer
+	LocalFree(pMsgBuf);
+	return errorString;
 }
 
-const char * Window::Exception::what() const noexcept
+
+Window::HrException::HrException(int line, const char* file, HRESULT hr) noexcept
+	:
+	Exception(line, file),
+	hr(hr)
+{}
+
+const char* Window::HrException::what() const noexcept
 {
 	std::ostringstream oss;
 	oss << GetType() << std::endl
-		<< "[Error Code]" << GetErrorCode() << std::endl
-		<< "[Description]" << GetErrorString() << std::endl
+		<< "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
+		<< std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
+		<< "[Description] " << GetErrorDescription() << std::endl
 		<< GetOriginString();
 	whatBuffer = oss.str();
 	return whatBuffer.c_str();
 }
 
-const char * Window::Exception::GetType() const noexcept
+const char* Window::HrException::GetType() const noexcept
 {
-	return "SkyEngine Exception Manager";
+	return "SkyEngine Window Exception";
 }
 
-std::string Window::Exception::TranslateErrorCode(HRESULT hr) noexcept
-{
-	char* pMsgBuf = nullptr;
-	DWORD nMsgLen = FormatMessage(
-		FORMAT_MESSAGE_ALLOCATE_BUFFER |
-		FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		nullptr, hr, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-		reinterpret_cast<LPSTR>(&pMsgBuf), 0, nullptr);
-	if (nMsgLen == 0)
-	{
-		return "Undefined error code.";
-	}
-	std::string errorString = pMsgBuf;
-	LocalFree(pMsgBuf);
-	return errorString;
-}
-
-HRESULT Window::Exception::GetErrorCode() const noexcept
+HRESULT Window::HrException::GetErrorCode() const noexcept
 {
 	return hr;
 }
 
-std::string Window::Exception::GetErrorString() const noexcept
+std::string Window::HrException::GetErrorDescription() const noexcept
 {
-	return TranslateErrorCode(hr);
+	return Exception::TranslateErrorCode(hr);
+}
+
+
+const char* Window::NoGfxException::GetType() const noexcept
+{
+	return "SkyEngine Window Exception [No Graphics]";
 }
--- SkyEngine/Window.h
+++ SkyEngine/Window.h
@@ -12,15 +12,26 @@ class Window
 public:
 	class Exception : public ExceptionManager
 	{
+		using ExceptionManager::ExceptionManager;
 	public:
-		Exception(int line, const char* file, HRESULT hr) noexcept;
-		const char* what() const noexcept override;
-		virtual const char* GetType() const noexcept;
 		static std::string TranslateErrorCode(HRESULT hr) noexcept;
+	};
+	class HrException : public Exception
+	{
+	public:
+		HrException(int line, const char* file, HRESULT hr) noexcept;
+		const char* what() const noexcept override;
+		const char* GetType() const noexcept override;
 		HRESULT GetErrorCode() const noexcept;
-		std::string GetErrorString() const noexcept;
+		std::string GetErrorDescription() const noexcept;
 	private:
 		HRESULT hr;
+	};
+	class NoGfxException : public Exception
+	{
+	public:
+		using Exception::Exception;
+		const char* GetType() const noexcept override;
 	};
 private:
 	class WindowClass
@@ -44,7 +55,7 @@ public:
 	Window(const Window&) = delete;
 	Window& operator=(const Window&) = delete;
 	void SetTitle(const std::string& title);
-	static std::optional<int> ProcessMessages();
+	static std::optional<int> ProcessMessages() noexcept;
 	Graphics& Gfx();
 private:
 	static LRESULT CALLBACK HandleMsgSetup(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lparam) noexcept;
@@ -60,6 +71,7 @@ private:
 	std::unique_ptr<Graphics> pGfx;
 };
 
-//error exception handler macro helper
-#define SKYENGINE_EXCEPT(hr) Window::Exception(__LINE__, __FILE__, hr)
-#define SKYENGINE_LAST_EXCEPT() Window::Exception(__LINE__, __FILE__, GetLastError())
+// error exception helper macro
+#define SKYENGINE_EXCEPT( hr ) Window::HrException( __LINE__,__FILE__,(hr) )
+#define SKYENGINE_LAST_EXCEPT() Window::HrException( __LINE__,__FILE__,GetLastError() )
+#define SKYENGINE_NOGFX_EXCEPT() Window::NoGfxException( __LINE__,__FILE__ )
