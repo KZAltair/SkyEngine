a691fe8a3d55ed60c5c7570736b857a67a87ad70
KZAltair <massyutin@hotmail.com>
2019-07-01 00:16:09 +0600

    Made hexagon and added indexed drawing

--- SkyEngine/App.cpp
+++ SkyEngine/App.cpp
@@ -29,8 +29,8 @@ void App::DoFrame()
 	//std::ostringstream oss;
 	//oss << "Time elapsed: " << std::setprecision(1) << std::fixed << t << "s";
 	//wnd.SetTitle(oss.str());
-	const float c = sin(timer.Peek()) / 2.0f + 0.5f;
-	wnd.Gfx().ClearBuffer(c, c, 1.0f);
+	//const float c = sin(timer.Peek()) / 2.0f + 0.5f;
+	wnd.Gfx().ClearBuffer(0.0f, 0.0f, 0.0f);
 	wnd.Gfx().DrawTestTriangle();
 	wnd.Gfx().EndFrame();
 }
--- SkyEngine/Graphics.cpp
+++ SkyEngine/Graphics.cpp
@@ -106,16 +106,31 @@ void Graphics::DrawTestTriangle()
 	//Create vertex type
 	struct Vertex
 	{
-		float x;
-		float y;
+		struct
+		{
+			float x;
+			float y;
+		}pos;
+		
+		struct
+		{
+			unsigned char r;
+			unsigned char g;
+			unsigned char b;
+			unsigned char a;
+		}color;
+		
 	};
 
 	//Fill in actual vertex data: position on the screen
 	const Vertex vertices[] =
 	{
-		{ 0.0f, 0.5f },
-		{ 0.5f, -0.5f },
-		{ -0.5f, -0.5f }
+		{ 0.0f, 0.5f, 255, 0, 0, 0 },
+		{ 0.5f, -0.5f, 0, 255, 0, 0 },
+		{ -0.5f, -0.5f, 0,0,255,0 },
+		{ -0.3f, 0.3f, 0,0,255,0 },
+		{ 0.3f, 0.3f, 0,0,255,0 },
+		{ 0.0f, -0.8f, 0,0,255,0 },
 	};
 
 	//Create buffer on GPU to store vertex data
@@ -136,11 +151,39 @@ void Graphics::DrawTestTriangle()
 
 	//Create buffer with all info above
 	GFX_THROW_INFO(pDevice->CreateBuffer(&bd, &sd, &pVertexBuffer));
+
+
 	const UINT stride = sizeof(Vertex);
 	const UINT offset = 0u;
 
 	//Add created vertex buffer with data to a pipeline
 	pContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset );
+
+	//Create index buffer
+	const unsigned short indices[] =
+	{
+		0,1,2,
+		0,2,3,
+		0,4,1,
+		2,1,5,
+	};
+
+	//Create index buffer
+	wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
+	D3D11_BUFFER_DESC ibd = {};
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.Usage = D3D11_USAGE_DEFAULT;
+	ibd.CPUAccessFlags = 0u;
+	ibd.MiscFlags = 0u;
+	ibd.ByteWidth = sizeof(indices);
+	ibd.StructureByteStride = sizeof(unsigned short);
+	//Add info of index data
+	D3D11_SUBRESOURCE_DATA isd = {};
+	isd.pSysMem = indices;
+	//Create buffer with all info above
+	GFX_THROW_INFO(pDevice->CreateBuffer(&ibd, &isd, &pIndexBuffer));
+
+	pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
 
 	//Create pixel shader
 	wrl::ComPtr<ID3D11PixelShader> pPixelShader;
@@ -165,7 +208,8 @@ void Graphics::DrawTestTriangle()
 	wrl::ComPtr<ID3D11InputLayout> pInputLayout;
 	const D3D11_INPUT_ELEMENT_DESC ied[] =
 	{
-		{"Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0}
+		{"Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+		{"Color", 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, 8u, D3D11_INPUT_PER_VERTEX_DATA, 0}
 	};
 	GFX_THROW_INFO(pDevice->CreateInputLayout(ied, (UINT)std::size(ied), pBlob->GetBufferPointer(), pBlob->GetBufferSize(), &pInputLayout));
 
@@ -188,7 +232,7 @@ void Graphics::DrawTestTriangle()
 	vp.TopLeftY = 0;
 	pContext->RSSetViewports(1u, &vp);
 
-	GFX_THROW_INFO_ONLY(pContext->Draw((UINT)std::size(vertices), 0u));
+	GFX_THROW_INFO_ONLY(pContext->DrawIndexed((UINT)std::size(indices), 0u, 0u));
 }
 
 Graphics::HrException::HrException(int line, const char * file, HRESULT hr, std::vector<std::string> infoMsgs) noexcept
